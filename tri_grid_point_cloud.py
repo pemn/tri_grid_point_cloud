ae1334e2d03dc9af6449bd865c2deda2c7759fce
Paulo Ernesto <pemn@users.noreply.github.com>
2019-08-07 11:01:18 -0300

    Add files via upload

--- tri_grid_point_cloud.py
+++ tri_grid_point_cloud.py
@@ -1,13 +1,14 @@
 #!python
 # create a surface/grid of a point cloud
 # input_path: file in one of the supported formats. x and y MUST be the first columns
+# variable: the variable to be averaged. Ex.: z.
 # cell_size: all points inside each cell of this size will be merged
 # output_path: result will be saved on this path with the format based on extension
 # v1.0 11/2017 paulo.ernesto
 # Copyright 2019 Vale
 # License: Apache 2.0
 """
-usage: $0 input_path*asc,csv,las,xlsx cell_size=1,2,5,10,25,50 output_path*00t,00g,csv
+usage: $0 input_path*asc,csv,las,xlsx variable=z cell_size=1,2,5,10,25,50 output_path*00t,00g,csv
 """
 
 import sys, os.path
@@ -17,7 +18,7 @@ import pandas as pd
 # import modules from a pyz (zip) file with same name as scripts
 sys.path.insert(0, os.path.splitext(sys.argv[0])[0] + '.pyz')
 
-from _gui import usage_gui, pd_load_dataframe
+from _gui import usage_gui, pd_load_dataframe, pd_save_dataframe
 
 from voxelspace import VoxelSpace
 
@@ -40,8 +41,10 @@ def pd_load_asc_grid(input_path):
 
 
 # convert a vulcan surface to a vulcan solid
-def tri_grid_point_cloud(input_path, cell_size, output_path):
+def tri_grid_point_cloud(input_path, variable, cell_size, output_path):
   cell_size = float(cell_size)
+  if variable:
+    variable = 'z'
 
   df = None
   if input_path.lower().endswith('.las'):
@@ -56,7 +59,8 @@ def tri_grid_point_cloud(input_path, cell_size, output_path):
 
   vs = VoxelSpace(df, [cell_size, cell_size])
 
-  vs.calculate_mean_xyz('z')
+  vs.calculate_mean_xyz(variable)
+
   if output_path.lower().endswith('.00t'):
     output_grid = output_path + '.00g'
     vs.save_vulcan_grid(output_grid)
@@ -64,14 +68,8 @@ def tri_grid_point_cloud(input_path, cell_size, output_path):
     os.remove(output_grid)
   elif output_path.lower().endswith('.00g'):
     vs.save_vulcan_grid(output_path)
-  elif output_path.lower().endswith('.csv'):
-    import csv
-    with open(output_path, 'w', newline='') as csvfile:
-      cw = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-      cw.writerow('xyz')
-      for i in range(vs.get_dx()):
-        for j in range(vs.get_dy()):
-          cw.writerow([i,j,vs[i,j]])
+  else:
+    pd_save_dataframe(pd.DataFrame(vs.dump_xyz(), columns=['x','y',variable]), output_path)
 
 main = tri_grid_point_cloud
 
